80b59ed7c27b66443c781853eb0d9f2fa9887076
Elia Dal Santo <Elia.DalSanto.GE1418@infocamere.it>
2017-04-04 17:48:51 +0200

    first commit

--- PranzoDoodle/Controllers/HomeController.cs
+++ PranzoDoodle/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PranzoDoodle.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,21 +11,108 @@ namespace PranzoDoodle.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            carpooltoolEntities entities = new carpooltoolEntities();
+
+            var sortedOptions = entities.PranzoOptions.ToList().OrderBy(o => o.defaultOption);
+
+            return View("PranzoView", new UsersAndOptions(entities.PranzoUsers, sortedOptions));
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+        [HttpPost]
+        public ActionResult EditSchedule(IEnumerable<PranzoUsers> users)
+        {
+            carpooltoolEntities entities = new carpooltoolEntities();
+
+            foreach (var user in users)
+            {
+                entities.PranzoUsers.Attach(user);
+                var entry = entities.Entry(user);
+                entry.Property(e => e.nome).IsModified = true;
+                entry.Property(e => e.preferenza).IsModified = true;
+            }
+
+            entities.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult AddUser(string userName)
         {
-            ViewBag.Message = "Your contact page.";
+            if (!string.IsNullOrWhiteSpace(userName))
+            {
+                carpooltoolEntities entities = new carpooltoolEntities();
+                try
+                {
+                    var option = (from o in entities.PranzoOptions where o.defaultOption == 1 select o).FirstOrDefault();
 
-            return View();
+                    entities.PranzoUsers.Add(new PranzoUsers() { nome = userName.Trim(), preferenza = option.id });
+                    entities.SaveChanges();
+                }
+                catch(Exception ex) {
+                    ex.ToString();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteUser(int userId)
+        {
+            carpooltoolEntities entities = new carpooltoolEntities();
+            var toRemove = (from u in entities.PranzoUsers where u.id == userId select u).FirstOrDefault();
+            if(toRemove != null)
+            {
+                try
+                {
+                    entities.PranzoUsers.Remove(toRemove);
+                    entities.SaveChanges();
+                }
+                catch { }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult AddOption(PranzoOptions option)
+        {
+            if (option != null && !string.IsNullOrWhiteSpace(option.descrizione))
+            {
+                carpooltoolEntities entities = new carpooltoolEntities();
+                try
+                {
+                    option.defaultOption = 0;
+                    entities.PranzoOptions.Add(option);
+                    entities.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Reset()
+        {
+            carpooltoolEntities entities = new carpooltoolEntities();
+            try
+            {
+                var option = (from o in entities.PranzoOptions where o.defaultOption == 1 select o).FirstOrDefault();
+
+                foreach (var user in entities.PranzoUsers)
+                {
+                    user.preferenza = option.id;
+                }
+                entities.SaveChanges();
+            }
+            catch { }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ PranzoDoodle/Models/UsersAndOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PranzoDoodle.Models
+{
+    public class UsersAndOptions
+    {
+        public IEnumerable<PranzoUsers> Users { get; set; }
+        public IEnumerable<PranzoOptions> Options { get; set; }
+
+        public UsersAndOptions(IEnumerable<PranzoUsers> users, IEnumerable<PranzoOptions> options)
+        {
+            Users = users;
+            Options = options;
+        }
+    }
+}
